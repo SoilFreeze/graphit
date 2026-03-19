2595b19864874da1e7be352d600c057beb795ed8
SoilFreeze <soilfreeze98072@gmail.com>
2026-03-19 08:29:31 -0700

    Update SF2329.py

--- SF2329.py
+++ SF2329.py
@@ -155,7 +155,7 @@ else:
         
         st.plotly_chart(fig, use_container_width=True, key="history_chart")
 
-   with tab3:
+    with tab3:
         # Use the selected location for the title (e.g., Thermal profile for TP-22-1)
         st.subheader(f"Thermal profile for {sel_loc}")
         
