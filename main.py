b10b70910315e0688b2381940bb60d88c4756289
SoilFreeze <soilfreeze98072@gmail.com>
2026-07-08 14:01:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,29 +13,9 @@ import io
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import plotly.express as px
-
-# 1. CONFIGURATION & STYLING
-st.set_page_config(
-    page_title="SoilFreeze Data Lab", 
-    page_icon="❄️", 
-    layout="wide"
-)
-
-# Global Database Constants - Linked to Read-Only Infrastructure
-DATASET_ID = "Temperature" 
-PROJECT_ID = "sensorpush-export"
-
-# Schema-Aligned Table References
-PROJECT_REGISTRY_TABLE = f"{PROJECT_ID}.{DATASET_ID}.project_registry"
-NODE_REGISTRY_TABLE = f"{PROJECT_ID}.{DATASET_ID}.node_registry"
-
-# THE UPGRADE: Pointing to the new flattened Phase/System view
-MASTER_VIEW = f"{PROJECT_ID}.{DATASET_ID}.master_data_view_v2" 
-REF_CURVE_TABLE = f"{PROJECT_ID}.{DATASET_ID}.reference_curves"
     
-###########################
+
 # - SIDEBAR NAVIGATION -  #
-###########################
 
 st.sidebar.title("❄️ SoilFreeze Lab")
 
