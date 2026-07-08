9b1f8ded9ff104375ce548b9e02a609969572bd9
SoilFreeze <soilfreeze98072@gmail.com>
2026-07-08 15:03:49 -0700

    Update config.py

--- app/utils/config.py
+++ app/utils/config.py
@@ -2,6 +2,6 @@
 DATASET_ID = "Temperature" 
 PROJECT_ID = "sensorpush-export"
 PROJECT_REGISTRY_TABLE = f"{PROJECT_ID}.{DATASET_ID}.project_registry"
-NODE_REGISTRY_TABLE = f"{PROJECT_ID}.{DATASET_ID}.node_registry"
+NODE_REGISTRY_TABLE = f"{PROJECT_ID}.{DATASET_ID}.node_registry_synced"
 MASTER_VIEW = f"{PROJECT_ID}.{DATASET_ID}.master_data_view_v2" 
 REF_CURVE_TABLE = f"{PROJECT_ID}.{DATASET_ID}.reference_curves"
